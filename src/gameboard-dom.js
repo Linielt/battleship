6938fbfd8a371de2cf0245941f2048dcb16ed7a6
Linielt <danielhlee912@gmail.com>
2024-09-25 23:01:42 +0100

    Displayed grids for two players

--- src/gameboard-dom.js
+++ src/gameboard-dom.js
@@ -1,19 +1,21 @@
 export const displayGameBoard = () => {
-  const gameBoardTable = document.getElementById("player-battleship-table");
+  const gameBoardTables = document.getElementsByClassName("battleship-table");
 
-  for (let y = 0; y < 10; y++) {
-    const gameBoardRow = document.createElement("tr");
-    gameBoardRow.className = "battelship-row";
-    for (let x = 0; x < 10; x++) {
-      const gameBoardCell = document.createElement("td");
-      gameBoardCell.className = "battleship-cell";
-      const gameBoardCellContent = document.createElement("div");
-      gameBoardCellContent.className = "battleship-cell-content";
-      gameBoardCellContent.setAttribute("data-x", String(x));
-      gameBoardCellContent.setAttribute("data-y", String(y));
-      gameBoardCell.appendChild(gameBoardCellContent);
-      gameBoardRow.appendChild(gameBoardCell);
+  for (let table of gameBoardTables) {
+    for (let y = 0; y < 10; y++) {
+      const gameBoardRow = document.createElement("tr");
+      gameBoardRow.className = "battelship-row";
+      for (let x = 0; x < 10; x++) {
+        const gameBoardCell = document.createElement("td");
+        gameBoardCell.className = "battleship-cell";
+        const gameBoardCellContent = document.createElement("div");
+        gameBoardCellContent.className = "battleship-cell-content";
+        gameBoardCellContent.setAttribute("data-x", String(x));
+        gameBoardCellContent.setAttribute("data-y", String(y));
+        gameBoardCell.appendChild(gameBoardCellContent);
+        gameBoardRow.appendChild(gameBoardCell);
+      }
+      table.appendChild(gameBoardRow);
     }
-    gameBoardTable.appendChild(gameBoardRow);
   }
 };
